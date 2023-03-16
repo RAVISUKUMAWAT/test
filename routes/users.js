bdd9e8088f046af7f2527057b1b15184a628d69a
ravi@stockal.com <ravi@stockal.com>
2023-03-16 08:21:01 +0530

    test .env

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const apiKey = process.env.API_KEY || '';
+  console.log("apiKey", apiKey);
+  res.send(apiKey);
 });
 
 module.exports = router;
