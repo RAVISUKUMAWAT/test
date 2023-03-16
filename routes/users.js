cb5830ebfefe5897354d1452b871e398f80746b0
ravi@stockal.com <ravi@stockal.com>
2023-03-16 08:48:07 +0530

    test json config

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,19 @@
 var express = require('express');
+const config = require('../config.json');
 var router = express.Router();
 
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env].database;
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/env', function(req, res, next) {
   const apiKey = process.env.API_KEY || '';
-  console.log("apiKey", apiKey);
-  res.send(apiKey);
+  res.send({ apiKey });
 });
+
+router.get('/json', function(req, res, next) {
+  res.send({ dbConfig });
+});
+
 
 module.exports = router;
